bcf1edb7427a7fc49561c3685e93d8abfdf1e46a
Hafizhun Soekiswo <hsoekiswo@gmail.com>
2025-01-16 05:40:17 +0800

    feat: initialize express

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import { Request, Response } from 'express';
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello Seruni!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
