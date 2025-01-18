de4721b8be0eaceea3b17634930089a4f90ec09e
Hafizhun Soekiswo <hsoekiswo@gmail.com>
2025-01-18 13:35:16 +0800

    fix: improve registration and login service

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import type { Request, Response } from 'express';
-import { generateAccessToken } from './service';
-import { password } from 'bun';
+import { registration, authenticateToken, generateAccessToken } from './service';
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello Seruni!');
 });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const users = [
-    {
-        'username': 'hsoekiswo',
-        'password': 'password',
-        'name': 'Hafizhun Soekiswo',
-        'email': 'hsoekiswo@gmail.com',
-        'phone': '08212345'
-    }
-]
 
 app.post('/register', (req: Request, res: Response) => {
     const data = req.body;
-    users.push(data);
+    const users = registration(data);
     res.json(users);
 })
 
@@ -34,7 +24,16 @@ app.post('/login', (req: Request, res: Response) => {
     const username = req.body.username;
     const password = req.body.password;
     const token = generateAccessToken(username, password);
+
+    if (!token) {
+        res.status(401).json({ message: 'Invalid username or password' });
+    }
+    
     res.json(token);
+});
+
+app.get('/dashboard', authenticateToken, (req: Request, res: Response) => {
+    res.send('Welcome to the dashboard');
 });
 
 app.listen(port, () => {
--- src/service.ts
+++ src/service.ts
@@ -1,13 +1,78 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import type { Request, Response, NextFunction } from 'express';
 
 dotenv.config();
 const secret = process.env.TOKEN_SECRET;
 
-export function generateAccessToken(username: any, password: any) {
-    const payload = {
-        username: username,
-        password: password
+export const users = [
+    {
+        'username': 'hsoekiswo',
+        'password': 'password',
+        'name': 'Hafizhun Soekiswo',
+        'email': 'hsoekiswo@gmail.com',
+        'phone': '08212345'
+    },
+    {
+        'username': 'ijun',
+        'password': 'ijun123',
+        'name': 'Ijun',
+        'email': 'ijun@mail.com',
+        'phone': '0898765'
     }
-    return jwt.sign(payload, secret, { expiresIn: '1800s' });
+]
+
+export function registration(data: any) {
+    users.push(data);
+    return data;
+}
+
+export function generateAccessToken(username: any, password: any) {
+    const user = users.find(user => user.username === username);
+
+    if (!user) {
+        console.error('User not found!');
+        return null;
+    }
+
+    if (user.password !== password) {
+        console.error('Invalid password');
+        return null; // or throw an error
+    }
+    
+    const payload = { username: user.username };
+    return jwt.sign(payload, secret as string, { expiresIn: '1800s' });
+};
+
+interface User {
+    username: string;
+    password: string;
+    iat: number;
+    exp: number;
+}
+  
+interface AuthenticatedRequest extends Request {
+    user?: User;
+};
+
+export const authenticateToken: any = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    console.log(token);
+  
+    if (token == null) {
+      return res.sendStatus(401);
+    }
+  
+    jwt.verify(token, process.env.TOKEN_SECRET as string, (err: any, payload: any) => {
+      console.log('Error:', err);
+  
+      if (err) {
+        return res.sendStatus(403);
+      }
+  
+      (req as AuthenticatedRequest).user = payload; // Cast req to AuthenticatedRequest
+  
+      next();
+    });
 };
