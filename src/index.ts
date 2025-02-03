8d90be2ffb7b78eafa7f52b85c3e3dc41165b70e
Hafizhun Soekiswo <hsoekiswo@gmail.com>
2025-02-03 15:12:14 +0800

    fix: cors typo

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ if (!existsSync("seruni.sqlite")) {
 
 // Configure CORS middleware to allow requests from your frontend
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://seruni-frontend.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://seruni-frontend.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
