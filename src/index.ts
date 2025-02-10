6248e78ee03d73d0cde37d3c072e623dc3ee1a26
Hafizhun Soekiswo <hsoekiswo@gmail.com>
2025-02-10 16:42:19 +0800

    fix: change URL string to env

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ if (!existsSync("seruni.sqlite")) {
 
 // Configure CORS middleware to allow requests from your frontend
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://seruni-frontend.vercel.app'],
+    origin: [process.env.URL_DEV as string, process.env.URL_PROD as string],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
